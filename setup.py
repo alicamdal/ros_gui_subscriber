b9845fe2783b4cbf79d6cf18b3c2840e416f51fa
alicamdal <alicamdal0@gmail.com>
2023-03-07 12:22:34 +0300

    update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='lent',
-    maintainer_email='alicamdal0@gmail.com',
+    maintainer_email='camdala@tcd.ie',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
